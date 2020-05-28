28af2bc03b8a5778c56a47906b3328a557a62026
tanya <60669157+tanyamiranda@users.noreply.github.com>
2020-05-28 15:20:25 -0400

    Updates

---
+++ precache-manifest.8d5685bf625dfd9beee4d77340c421c8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2f8b7bac9d13b09ee96ba8e52a10685",
+    "url": "/memory-game/index.html"
+  },
+  {
+    "revision": "c4cf48e17c455d3b2f88",
+    "url": "/memory-game/static/css/main.654e68f7.chunk.css"
+  },
+  {
+    "revision": "387e0087f0c585cd8718",
+    "url": "/memory-game/static/js/2.d3525540.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/memory-game/static/js/2.d3525540.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4cf48e17c455d3b2f88",
+    "url": "/memory-game/static/js/main.9669b662.chunk.js"
+  },
+  {
+    "revision": "a7295d09a3c7d39f3388",
+    "url": "/memory-game/static/js/runtime-main.cd8959cd.js"
+  },
+  {
+    "revision": "f64fdd9fac0aaab45a604b7d9b800bda",
+    "url": "/memory-game/static/media/logo-blue-green-black.f64fdd9f.svg"
+  },
+  {
+    "revision": "d6f0889164e492bc9e1c6eebd5cc3471",
+    "url": "/memory-game/static/media/logo-card-backs.d6f08891.svg"
+  }
+]);
