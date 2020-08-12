ced58a975a7364109964af1894178afa4232ae15
tanya <60669157+tanyamiranda@users.noreply.github.com>
2020-08-12 14:46:32 -0400

    Moved index.css to public to try to address url() issue with PWA where background svg is not appearing.

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
-import './index.css';
-
 import MemoryGame from './pages/memory-game/memory-game.component';
 import {store} from './redux/store';
 
